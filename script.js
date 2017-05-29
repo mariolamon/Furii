822fa7686bdf4c6ce1275e4bb5b4999ccf788b86
mariolamon <mario.vanhovelamon@gmail.com>
2017-05-29 22:35:20 +0200

    base et moteur du jeux

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.requestAnimFrame = (function() {
 		window.oRequestAnimationFrame 		||
 		window.msRequestAnimationFrame 		||
 		function (callback) {
-			return window.setTimeout(callback, 1000 / Fps);
+			return window.setTimeout(callback, 1000 / 120);
 		});
 })();
 window.cancelRequestAnimFrame = (function() {
@@ -23,69 +23,124 @@ let	canvas = document.getElementById("canvas"),
 	H = 600,
 	W = 800,
 	
-	StartBtn = {},
-	RestartBtn = {},
+	startBtn = {},
+	restartBtn = {},
 	mouse = {},
-	
-	Background = {},
-	BackgroundStart = new Image(),
-	BackgroundLevel01 = new Image(),
-	
-	Player = {},
-	PlayerNormal = new Image(),
-	PlayerShot = new Image(),
-	
-	Point = 0,
-	Life = 100,
-	Level = 0,
-	Fps = 60,
-	Ball = {},
-	Blood = {},
-	BloodCount = 20,
-	Over = 0,
-	sound = {},
-	Init;
 
-canvas.addEventListener("mousemove", TrackPosition, true);
-canvas.addEventListener("mousedown", BtnClick, true);
+	viewfinder = {},
+	viewNormal = new Image(),
+	viewRed = new Image(),
+	
+	background = {},
+	backgroundStart = new Image(),
+	backgroundLevel01 = new Image(),
+	
+	player = {},
+	playerNormal = new Image(),
+	playerShot = new Image(),
+	
+	balls = [],
+	ball = {},
+	wall = [],
+	furniture = [],
+
+	up,
+	down,
+	left,
+	right,
+
+	shot = false,
+
+	point = 0,
+	life = 100,
+	lvl = 0,
+	blood = {},
+	bloodCount = 20,
+	over = 0,
+	sound = {},
+	init;
+
+canvas.addEventListener("mousemove", trackPosition, true);
+canvas.addEventListener("mousedown", click, true);
+canvas.addEventListener("mouseup", function() { if (lvl != 0) shot = false; }, true);
 window.onkeydown = KeyDown;
 
 canvas.height = H;
 canvas.width = W;
 
-Mouse = {x: 0, y: 0};
+wall = [
+	[{w: 790, h: 7, x: 5, y: 5}, {w: 7, h: 590, x: 5, y: 5},
+	{w: 790, h: 7, x: 5, y: 588}, {w: 7, h: 590, x: 788, y: 5},
+	{w: 64, h: 7, x: 12, y: 211}, {w: 113, h: 7, x: 157, y: 211},
+	{w: 7, h: 206, x: 263, y: 12}, {w: 7, h: 306, x: 264, y: 282},
+	{w: 64, h: 7, x: 12, y: 282}, {w: 113, h: 7, x: 157, y: 282},
+	{w: 13, h: 13, x: 575, y: 315}, {w: 7, h: 14, x: 578, y: 573}]
+];
+furniture = [
+	[{w: 133, h: 45, x: 16, y: 21}, {w: 12, h: 54, x: 12, y: 92},
+	{w: 37, h: 16, x: 213, y: 15}, {w: 39, h: 80, x: 224, y: 131},
+	{w: 100, h: 133, x: 87, y: 455}, {w: 39, h: 35, x: 33, y: 548},
+	{w: 39, h: 35, x: 200, y: 547}, {w: 136, h: 169, x: 355, y: 330},
+	{w: 38, h: 19, x: 391, y: 311}, {w: 107, h: 37, x: 157, y: 288},
+	{w: 20, h: 37, x: 335, y: 341}, {w: 10, h: 38, x: 345, y: 387},
+	{w: 12, h: 38, x: 343, y: 432}, {w: 12, h: 38, x: 343, y: 474},
+	{w: 12, h: 38, x: 466, y: 348}, {w: 16, h: 38, x: 466, y: 394},
+	{w: 10, h: 38, x: 466, y: 439}, {w: 12, h: 38, x: 466, y: 481},
+	{w: 37, h: 124, x: 563, y: 408}, {w: 55, h: 152, x: 733, y: 436},
+	{w: 60, h: 64, x: 660, y: 523}, {w: 51, h: 48, x: 737, y: 386}]	
+];
 
-ball = {x: 0, y: 0};
-
-PlayerNormal.src = "img/Player.png";
-PlayerShot.src = "img/PlayerShot.png";
-Player = {
+playerNormal.src = "img/Player.png";
+playerShot.src = "img/PlayerShot.png";
+player = {
 	w: 55, h: 46, x: (W / 2 - 46), y: (H / 2 - 55), a: 0,
-	draw: function(type) {
+	draw: function() {
 		ctx.save();
 		ctx.translate(this.x, this.y);
 		ctx.rotate(this.a);
-		if (type == "normal") ctx.drawImage(PlayerNormal, -(this.w / 2), -(this.h / 2));
-		if (type == "shot") {
-			ctx.drawImage(PlayerShot, -(this.w / 2), 0);	
-		}
+		if (shot == false) ctx.drawImage(playerNormal, -(this.w / 2), -(this.h / 2));
+		else if (shot == true) ctx.drawImage(playerShot, -(this.w / 2), -(this.h / 2));
 		ctx.restore();
 	}
 };
 
-BackgroundStart.src = "img/BackgroundMain.png";
-BackgroundLevel01.src = "img/BackgroundLevel01.png";
-Background = {
+backgroundStart.src = "img/BackgroundMain.png";
+backgroundLevel01.src = "img/BackgroundLevel01.png";
+background = {
 	w: 800, h:600, x: 0, y: 0,
 	draw: function() {
 		ctx.save();
-		if (Over == 1) ctx.drawImage(BackgroundStart, this.x, this.y);
-		else ctx.drawImage(BackgroundLevel01, this.x, this.y);
+		if (over == 1) ctx.drawImage(backgroundStart, this.x, this.y);
+		else ctx.drawImage(backgroundLevel01, this.x, this.y);
 		ctx.restore();
 	}
 };
 
-StartBtn = {
+viewNormal.src = "img/Viewfinder.png";
+viewRed.src = "img/ViewfinderRed.png";
+viewfinder = {
+	w: 21, h: 21, x: 0, y: 0,
+	draw: function() {
+		ctx.save();
+		ctx.drawImage(viewNormal, this.x - 10, this.y - 10);
+		ctx.restore();
+	}
+};
+
+ball = {
+	w: 10, h: 2, draw: function() {
+		for (let i = 0; balls[i]; i++) {
+			ctx.save();
+			ctx.translate(balls[i].x, balls[i].y);
+			ctx.rotate(balls[i].a);
+			ctx.fillStyle = "#fbdd8d";
+			ctx.fillRect(player.w / 2, 16, this.w, this.h);
+			ctx.restore();
+		}
+	}
+};
+
+startBtn = {
 	w:  100, h: 40, x: (W / 2 - 50), y: (H / 2 - 25),
 	draw: function() {
 		ctx.save();
@@ -100,7 +155,7 @@ StartBtn = {
 		ctx.restore();
 	}
 };
-RestartBtn = {
+restartBtn = {
 	w: 100, h: 50, x: (W / 2 - 50), y: (H / 2 - 50),
 	draw: function() {
 		ctx.save();
@@ -116,45 +171,81 @@ RestartBtn = {
 	}
 };
 
-function Draw() {
-	Background.draw();
-	Player.draw("normal");
-	Update();
-};
-
-function TrackPosition (event) {
-	Mouse.x = event.pageX;
-	Mouse.y = event.pageY;
-};
-
-function Update() {
-	if (Mouse.x && Mouse.y) {
-		Player.a = Math.atan2(Mouse.y - Player.y, Mouse.x - Player.x);
+function collide(elementA, elementB) {
+	let index;
+	let aX, aY, aW, aH, bX, bY, bW, bH;
+	if (elementA == player) {
+		aX = elementA.x - (elementA.w / 2);
+		aY = elementA.y - (elementA.h / 2);
+		aW = elementA.w;
+		aH = elementA.h;
 	}
-	console.log("update");
-	if (Life == 0) GamOver();
-	UpdateLife();	
+	if ((elementB == wall) || (elementB == furniture)) index = lvl - 1;
+	for (let i = 0; elementB[index][i]; i++) {
+		bX = elementB[index][i].x;
+		bY = elementB[index][i].y;
+		bW = elementB[index][i].w;
+		bH = elementB[index][i].h;
+		if ((aX >= bX) && (aX <= (bX + bW)) || (((aX + aW) >= bX) && (aX <= bX))) {
+			if ((aY >= (bY + (bH - 6))) && (aY <= (bY + bH))) up = true;
+			if (((aY + aH) >= bY) && ((aY + aH) <= (bY + 6))) down = true;
+		}
+		if ((aY >= bY) && (aY <= (bY + bH)) || (((aY + aH) >= bY) && (aY <= bY))) {
+			if ((aX >= (bX + (bW - 6))) && (aX <= (bX + bW))) left = true;
+			if (((aX + aW) >= bX) && ((aX + aW) <= (bX + 6))) right = true;
+		}
+	}
 };
-function UpdateScore() {
+
+function wallDraw() {
+	ctx.save();
+	ctx.fillStyle = "red";
+	for (let i = 0; wall[0][i]; i++) ctx.fillRect(wall[0][i].x, wall[0][i].y, wall[0][i].w, wall[0][i].h);
+	ctx.restore();
+};
+
+function draw() {
+	background.draw();
+	ball.draw();
+	player.draw();
+	viewfinder.draw();
+	update();
+};
+
+function trackPosition (event) {
+	mouse.x = event.pageX;
+	mouse.y = event.pageY;
+};
+
+function update() {
+	if (mouse.x && mouse.y) {
+		player.a = Math.atan2(mouse.y - player.y, mouse.x - player.x);
+		viewfinder.x = mouse.x;
+		viewfinder.y = mouse.y;
+	}
+	if (life == 0) GamOver();
+	updateLife();	
+};
+function updateScore() {
 	ctx.save();
 	ctx.fillStyle = "#fff";
 	ctx.font = "20px 'btn',arial,sans-serif";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Score: " + Point, 20, 20 );
+	ctx.fillText("Score: " + point, 20, 20 );
 	ctx.restore();
 };
-function UpdateLife() {
+function updateLife() {
 	let color;
-	if (Life <= 25) color = "#DC143C";
-	if ((Life <= 50) && (Life > 25)) color = "#FFD700";
+	if (life <= 25) color = "#DC143C";
+	else if ((life <= 50) && (life > 25)) color = "#FFD700";
 	else color = "#008000";
 	ctx.save();
 	ctx.strokeStyle = "#ffffff";
 	ctx.fillStyle = color;
 	ctx.lineWidth = "2";
 	ctx.strokeRect(670, 555, 102, 17);
-	ctx.fillRect(671, 556, Life, 15);
+	ctx.fillRect(671, 556, life, 15);
 	ctx.fillStyle = "#ffffff";
 	ctx.fillRect(650, 554, 19, 19);
 	ctx.fillStyle = "#DC143C";
@@ -163,49 +254,59 @@ function UpdateLife() {
 	ctx.restore();
 };
 
-function StartScreen() {
-	StartBtn.draw();
-};
-StartScreen();
+function startScreen() {
+	startBtn.draw();
+} startScreen();
 
-function BtnClick(event) {
+function click(event) {
 	let mx = event.pageX, my = event.pageY;
-	if ((mx >= StartBtn.x && mx <= StartBtn.x + StartBtn.w) && (my >= StartBtn.y && my <= StartBtn.y + StartBtn.h)) {
-		AnimLoop();
-		StartBtn = {};
-		Level = 1;
+	if (lvl != 0) {
+		balls.push({x: player.x, y: player.y, a: player.a});
+		shot = true;
 	}
-	if (Over == 1) {
-		if ((mx >= RestartBtn.x && mx <= RestartBtn.x + RestartBtn.w) && (my >= RestartBtn.y && my <= RestartBtn.y + RestartBtn.h)) {
-			AnimLoop();
-			Over = 0;
-			Level = 1;
-			Point = 0;
-			Life = 100;
+	if ((mx >= startBtn.x && mx <= startBtn.x + startBtn.w) && (my >= startBtn.y && my <= startBtn.y + startBtn.h)) {
+		animLoop();
+		startBtn = {};
+		lvl = 1;
+		canvas.style.cursor = "none";
+	}
+	if (over == 1) {
+		if ((mx >= restartBtn.x && mx <= restartBtn.x + restartBtn.w) && (my >= restartBtn.y && my <= restartBtn.y + restartBtn.h)) {
+			animLoop();
+			over = 0;
+			lvl = 1;
+			point = 0;
+			life = 100;
 		}
 	}
 };
-function AnimLoop() {
-	Init = requestAnimFrame(AnimLoop);
-	Draw();
+
+function animLoop() {
+	init = requestAnimFrame(animLoop);
+	draw();
 };
-function GameOver() {
+
+function gameOver() {
 	ctx.save();
 	ctx.font = "20px 'over',arial,sans-serif";
 	ctx.textAlign = "center";
 	ctx.textBaseligne = "middle";
 	ctx.fillStyle = "#fff";
-	ctx.fillText("Game Over - You scored "+Point+" points !", (W / 2), (H / 2 + 25));
+	ctx.fillText("Game Over - You scored " + point + " points !", (W / 2), (H / 2 + 25));
 	ctx.restore();
-	cancelRequestAnimFrame(Init);
-	Over = 1;
-	RestartBtn.draw();
+	cancelRequestAnimFrame(init);
+	over = 1;
+	restartBtn.draw();
 };
 
 KEY_DOWN	= 40;
 KEY_UP		= 38;
 KEY_LEFT	= 37;
 KEY_RIGHT	= 39;
+KEY_Z		= 90;
+KEY_S		= 83;
+KEY_Q		= 81;
+KEY_D		= 68;
 
 function CheckEvent(event) {
 	if (window.event) return window.event;
@@ -213,10 +314,17 @@ function CheckEvent(event) {
 };
 function KeyDown(event) {
 	let WinObject = CheckEvent(event);
-	if (!(Level == 0)) {
-		if (WinObject.keyCode == KEY_RIGHT) Player.x += 4;
-		if (WinObject.keyCode == KEY_LEFT) Player.x -= 4;
-		if (WinObject.keyCode == KEY_UP) Player.y -= 4;
-		if (WinObject.keyCode == KEY_DOWN) Player.y += 4;
+	let key = WinObject.keyCode;
+	up = false;
+	down = false;
+	left = false;
+	right = false;
+	collide(player, wall);
+	collide(player, furniture);
+	if (lvl != 0) {
+		if ((key == KEY_RIGHT || key == KEY_D) && (right != true)) player.x += 6;
+		if ((key == KEY_LEFT || key == KEY_Q) && (left != true)) player.x -= 6;
+		if ((key == KEY_UP || key == KEY_Z) && (up != true)) player.y -= 6;
+		if ((key == KEY_DOWN || key == KEY_S) && (down != true)) player.y += 6;
 	}
 };
